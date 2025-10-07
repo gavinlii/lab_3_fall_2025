e84184e064f4aba619dc1843def90344f19377c6
gavinlii <gavgriffin563@gmail.com>
2025-10-07 02:13:23 +0100

    correct right FK

--- lab_3.py
+++ lab_3.py
@@ -107,11 +107,11 @@ class InverseKinematics(Node):
 
         # T_1_2 (leg_front_l_1 to leg_front_l_2)
         ## TODO: Implement the transformation matrix from leg_front_l_1 to leg_front_l_2
-        T_1_2 = translation(0, 0, 0.039) @ rotation_y(1.57080) @ rotation_z(theta2)
+        T_1_2 = translation(0, 0, 0.039) @ rotation_y(-1.57080) @ rotation_z(theta2)
 
         # T_2_3 (leg_front_l_2 to leg_front_l_3)
         ## TODO: Implement the transformation matrix from leg_front_l_2 to leg_front_l_3
-        T_2_3 = translation(0, -0.0494, 0.0685) @ rotation_y(-1.57080) @ rotation_z(theta3)
+        T_2_3 = translation(0, -0.0494, 0.0685) @ rotation_y(1.57080) @ rotation_z(theta3)
 
         # T_3_ee (leg_front_l_3 to end-effector)
         T_3_ee = translation(0.06231, -0.06216, 0.018) 
